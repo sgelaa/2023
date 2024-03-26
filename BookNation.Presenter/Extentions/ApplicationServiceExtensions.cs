73dcb1349a10a17ff89f0f1c298c3e16c04d3a32
retrotshepo <tshepo.mabine@dariel.co.za>
2024-03-26 09:34:43 +0200

    .

--- BookNation.Presenter/Extentions/ApplicationServiceExtensions.cs
+++ BookNation.Presenter/Extentions/ApplicationServiceExtensions.cs
@@ -16,19 +16,16 @@ namespace BookNation.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services,
         IConfiguration config)
         {
-            // "Server=(localdb)\\mssqllocaldb;Database=ArmsCore;Trusted_Connection=True;MultipleActiveResultSets=true"
-            // "Server=(localdb)\\mssqllocaldb;Database=ArmsCoreGlobal;Trusted_Connection=True;MultipleActiveResultSets=true"
             services.AddDbContext<DataContext>(opt =>
             {
                 // opt.UseSqlite(config.GetConnectionString("SqliteConnection"));
-                // opt.UseSqlServer(config.GetConnectionString("SqlServerDarConnection"));
                 opt.UseSqlServer(config.GetConnectionString("SqlServerLocalConnection"));
             });
 
             services.AddScoped<ICustomerRepository, CustomerRepository>();
             services.AddScoped<IUsersRepository, UsersRepository>();
-            
-            
+
+
             services.AddScoped<ICustomerService, CustomerService>();
             services.AddScoped<IUsersService, UsersService>();
 
@@ -36,7 +33,7 @@ namespace BookNation.Extensions
             // services.AddScoped<ICustomerService, CustomerService>();
 
             services.AddCors();
-            
+
             services.AddScoped<ITokenService, TokenService>();
 
             return services;
