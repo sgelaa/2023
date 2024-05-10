e1666212368ad7a0394e790320aff10d5b11afef
retrotshepo <tshepo.mabine@dariel.co.za>
2024-05-10 14:24:59 +0200

    migrations update

---
+++ BookNation.DataAccess/Data/Migrations/20240510122401_AppUserUpdate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookNation.DataAccess.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AppUserUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Resources_Users_AppUserId",
+                table: "Resources");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Resources_AppUserId",
+                table: "Resources");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Resources_AppUserId",
+                table: "Resources",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Resources_Users_AppUserId",
+                table: "Resources",
+                column: "AppUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
